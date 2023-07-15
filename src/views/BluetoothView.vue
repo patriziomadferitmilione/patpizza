87da7a103d89589d37ab71abf75960b3fc0f9ed8
Patrizio Milione <patriziomilione@gmail.com>
2023-07-15 15:58:38 +0200

    bluetooth

--- src/views/BluetoothView.vue
+++ src/views/BluetoothView.vue
@@ -5,7 +5,38 @@
       <!-- Display device list and other relevant information -->
       <ul>
         <li v-for="device in devices" :key="device.id">
-          {{ device.name }} - {{ device.id }}
+          <div>
+            <strong>Name:</strong> {{ device.name }}
+          </div>
+          <div>
+            <strong>Address:</strong> {{ device.address }}
+          </div>
+          <div>
+            <strong>Manufacturer:</strong> {{ device.manufacturerName }}
+          </div>
+          <div>
+            <strong>Product Name:</strong> {{ device.productName }}
+          </div>
+          <div>
+            <strong>UUIDs:</strong>
+            <ul>
+              <li v-for="uuid in device.uuids" :key="uuid">{{ uuid }}</li>
+            </ul>
+          </div>
+          <!-- Add any other desired properties from the device object -->
+          <div>
+            <strong>Services:</strong>
+            <ul>
+              <li v-for="service in device.services" :key="service.uuid">
+                {{ service.uuid }}
+                <ul>
+                  <li v-for="characteristic in service.characteristics" :key="characteristic.uuid">
+                    {{ characteristic.uuid }}
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </li>
       </ul>
     </div>
@@ -15,7 +46,7 @@
   import { ref } from 'vue';
   
   export default {
-    name: 'BluetoothComponent',
+    name: 'BluetoothView',
     setup() {
       const devices = ref([]);
   
@@ -26,15 +57,33 @@
             acceptAllDevices: true,
           });
           // Extract relevant information from the devices
-          const deviceList = devices.map(device => ({
-            name: device.name,
-            id: device.id,
-            address: device.address,
-            uuid: device.uuids[0],
-            // Add any other desired properties
-          }));
+          const deviceList = devices.map(async device => {
+            // Get primary services
+            const services = await device.getPrimaryServices();
+  
+            // Extract characteristic information for each service
+            const serviceList = await Promise.all(
+              services.map(async service => {
+                const characteristics = await service.getCharacteristics();
+                return {
+                  uuid: service.uuid,
+                  characteristics: characteristics.map(c => ({ uuid: c.uuid })),
+                };
+              })
+            );
+  
+            // Return device information including services and characteristics
+            return {
+              name: device.name,
+              address: device.address,
+              manufacturerName: device.manufacturerName,
+              productName: device.productName,
+              uuids: device.uuids,
+              services: serviceList,
+            };
+          });
           // Update devices ref
-          devices.value = deviceList;
+          devices.value = await Promise.all(deviceList);
         } catch (error) {
           console.error('Error:', error);
         }
@@ -42,50 +91,7 @@
   
       // Trigger the Bluetooth speaker
       const triggerSpeaker = async () => {
-        try {
-          const device = await navigator.bluetooth.requestDevice({
-            filters: [{ services: ['audio_sink'] }],
-          });
-  
-          const server = await device.gatt.connect();
-  
-          // Access the primary service
-          const service = await server.getPrimaryService('audio_sink');
-          console.log('Service:', service);
-  
-          // Access the characteristic for triggering the speaker
-          const characteristic = await service.getCharacteristic('trigger_characteristic');
-          console.log('Characteristic:', characteristic);
-  
-          // Read characteristic value
-          const value = await characteristic.readValue();
-          console.log('Value:', value);
-  
-          // Write characteristic value
-          const newValue = new Uint8Array([0x01]);
-          await characteristic.writeValue(newValue);
-          console.log('Value written successfully');
-  
-          // Subscribe to characteristic value changes
-          characteristic.addEventListener('characteristicvaluechanged', (event) => {
-            const value = event.target.value;
-            console.log('New value:', value);
-          });
-  
-          characteristic.startNotifications()
-            .then(() => {
-              console.log('Subscribed to characteristic value changes');
-            })
-            .catch((error) => {
-              console.error('Error:', error);
-            });
-  
-          await server.disconnect();
-  
-          console.log('Speaker triggered successfully');
-        } catch (error) {
-          console.error('Error:', error);
-        }
+        // Your existing triggerSpeaker code here
       };
   
       return {
